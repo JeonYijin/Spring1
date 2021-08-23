24c4114e02e70ec90c88f509e37e093e2965ce0c
JeonYijin <juan6016@naver.com>
2021-08-23 22:01:45 +0900

    Initial commit

---
+++ src/main/java/com/ae/ae1/SubTest.java
@@ -0,0 +1,14 @@
+package com.ae.ae1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubTest {
+	
+	public void sub() {
+		System.out.println("SubTest의 sub 메서드");
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/ae/ae1/TestDAO.java
@@ -0,0 +1,16 @@
+package com.ae.ae1;
+
+public class TestDAO {
+
+	
+	public void getTest() {
+		//DB연결
+		//member 테이블의 모든것 조회해서 출력
+		
+		String user = "user02";
+		String pw = "user02";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		
+		
+	}
+}
